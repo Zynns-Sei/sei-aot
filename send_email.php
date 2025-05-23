3ba33e6043a58ecadbae8310461266fab422d8e3
Zynns-Sei <zynnssei@gmail.com>
2025-05-23 13:30:09 +0630

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+    
+    // Set recipient email (replace with your email)
+    $recipient = "aunghpounmyat@gmail.com";
+    
+    // Set email subject
+    $email_subject = "New contact from AOT site: $subject";
+    
+    // Build email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+    
+    // Build email headers
+    $email_headers = "From: $name <$email>";
+    
+    // Send email
+    if (mail($recipient, $email_subject, $email_content, $email_headers)) {
+        http_response_code(200);
+        echo "Thank you! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
